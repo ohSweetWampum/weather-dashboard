d82d47949020879f1851272769451560110f7f62
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-06 21:57:38 -0500

    made html using grid bootstrap

--- script.js
+++ script.js
@@ -1,3 +1,123 @@
+
+// //need to make a search bar on the left hand side (aside) of site with a search button and a space below for previous searchs to appear as buttons that you can click on to see there info, presumably alot of this can be done with bootstrap. need a header that says weatehr dashboard, need current day weather below that, need 5 day below that. Need to store user input to local storage and be able to retrieve it.
+// //need to use geocoding api, then link that up with the 5 day/3 hour forecast api
+
+
+
+
+// //save user input to local storage
+function retrieveGeocoordinates(){
+  var cityCoordinates = $(this).attr("data-cityCoordinates");
+  var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={2e7ad0266ed734e297938c3bcc22afc5}"
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $("#movies-view").empty();
+
+   
+
+
+
+
+// create button for a recent searched city
+
+function displayWeatherInfoForRecentSearch() {
+        
+  var cityName = $(this).attr("data-cityName");
+  var queryURL = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={ 2e7ad0266ed734e297938c3bcc22afc5}
+
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $("#movies-view").empty();
+
+    // Creating a div to hold the movie
+    // var movieDiv = $("<div class='movie'>");
+    var movieDiv = $("<div>");
+    movieDiv.attr("class", 'movie');
+
+    console.log(response);
+    // Storing the rating data
+    var rating = response.Rated;
+
+    // Creating an element to have the rating displayed
+    var pOne = $("<p>");
+
+    pOne.text("Rating: " + rating);
+   
+    // Displaying the rating
+    movieDiv.append(pOne);
+
+    // Storing the release year
+    var released = response.Released;
+
+    // Creating an element to hold the release year
+    var pTwo = $("<p>")
+    pTwo.text("Released: " + released);
+
+    // Displaying the release year
+    movieDiv.append(pTwo);
+
+    // Storing the plot
+    var plot = response.Plot;
+
+    // Creating an element to hold the plot
+    var pThree = $("<p>")
+    pThree.text("Plot: " + plot);
+
+    // Appending the plot
+    movieDiv.append(pThree);
+
+    // Retrieving the URL for the image
+    var imgURL = response.Poster;
+
+    // Creating an element to hold the image
+    var image = $("<img>").attr("src", imgURL);
+
+    // Appending the image
+    movieDiv.append(image);
+
+  
+
+    // Putting the entire movie above the previous movies
+    $("#movies-view").prepend(movieDiv);
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //display current day at top of the page
 function displayCurrentDateAtTop() {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY h:mm a"));
