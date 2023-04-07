0b914659bbeec9ea598cdf9e61699154fd03878b
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-06 22:48:22 -0500

    added 2nd section, footer and added search bar and button in the aside element

--- script.js
+++ script.js
@@ -3,8 +3,52 @@
 // //need to use geocoding api, then link that up with the 5 day/3 hour forecast api
 
 
+  // Function for displaying movie data
+  function renderSearchHistoryButtons() {
+
+    // Deleting the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#createButton").empty();
+
+    // Looping through the array of movies
+    for (var i = 0; i < movies.length; i++) {
+
+      // Then dynamicaly generating buttons for each movie in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      var newButton = $("<button>");
+      // Adding a class of movie-btn to our button
+      newButton.addClass("movie-btn");
+      // Adding a data-attribute
+      newButton.attr("data-name", movies[i]);
+      // Providing the initial button text
+      newButton.text(movies[i]);
+      // Adding the button to the buttons-view div
+      $("#createButton").append(newButton);
+    }
+  }
+
+  // This function handles events where a movie button is clicked
+  $("#add-movie").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var movie = $("#movie-input").val().trim();
+
+    // Adding movie from the textbox to our array
+    movies.push(movie);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderSearchHistoryButtons();
+  });
+
+  // Adding a click event listener to all elements with a class of "movie-btn"
+  $(document).on("click", ".movie-btn", displayMovieInfo);
+
+  // Calling the renderButtons function to display the intial buttons
+  renderSearchHistoryButtons();
 
 
+
+  
 // //save user input to local storage
 function retrieveGeocoordinates(){
   var cityCoordinates = $(this).attr("data-cityCoordinates");
