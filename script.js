b93c11d13116ee72beeab577ab3d92736aacec7f
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-09 18:56:16 -0500

    made the current weather display on the screen, mostly javascript work

--- script.js
+++ script.js
@@ -4,32 +4,54 @@
 
 
 //Search button search functionality, call api, return current and 5 day forecast
-var searchButton = document.getElementById("search-city");
 
-searchButton.addEventListener("submit", function(event) {
+var searchInputBox = document.getElementById("searchBox");
+searchInputBox.addEventListener("submit", function(event) {
   event.preventDefault();
   
   var cityInput = document.getElementById("inputCityName").value;
   var apiKey = "2e7ad0266ed734e297938c3bcc22afc5";
   var apiURLCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + apiKey;
-  
+  var currentTemp = document.getElementById("current-temp");
+  var currentDayConditions = document.getElementById("current-day-conditions");
+  var currentDayHigh = document.getElementById("current-day-high");
+  var currentDayLow = document.getElementById("current-day-low");
+  var currentDayWind = document.getElementById("current-day-wind");
   fetch(apiURLCurrentWeather)
     .then(response => response.json())
     .then(data => {
       var lat = data.coord.lat;
       var lon = data.coord.lon;
       var apiURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+      currentTemp.textContent = "Temperature: " + data.main.temp + "°F";
+      currentDayHigh.textContent = "High: " + data.main.temp_max + "°F";
+      currentDayLow.textContent = "Low: " + data.main.temp_min + "°F";
+      currentDayWind.textContent = "Wind Speed: " + data.wind.speed + " mph";
+
+      // Update city name and current date
+      var cityName = data.name;
+      var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+      var cityTitle = document.getElementById("city-title");
+      var iconCode = data.weather[0].icon;
+      var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+      cityTitle.innerHTML =   cityName + '<img src="' + iconUrl + '">' + currentDate;
+      
+      
+      
+
+
+      renderRecentlySearchedButtons(cityInput);
 
       fetch(apiURLFiveDay)
         .then(response => response.json())
         .then(data => {
-          console.log(data);
+          
           
         })
         .catch(error => console.error(error));
     })
     .catch(error => console.error(error));
-    console.log(data.list);
+    
 });
 
 
@@ -38,67 +60,35 @@ searchButton.addEventListener("submit", function(event) {
 
 
   // Function for rendering recently searched location buttons
-  function renderRecentlySearchedButtons() {
-    var 
-
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#createButton").empty();
-
-    // Looping through the array of movies
-    for (var i = 0; i < movies.length; i++) {
-
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var newButton = $("<button>");
-      // Adding a class of movie-btn to our button
-      newButton.addClass("movie-btn");
-      // Adding a data-attribute
-      newButton.attr("data-name", movies[i]);
-      // Providing the initial button text
-      newButton.text(movies[i]);
-      // Adding the button to the buttons-view div
-      $("#createButton").append(newButton);
+  function renderRecentlySearchedButtons(cityInput) {
+    var cityButtonContainer = document.getElementById("recent-cities-buttons-container");
+  
+    // check if city has already been searched
+    var isDuplicate = false;
+    var buttons = cityButtonContainer.querySelectorAll('button');
+    buttons.forEach(function(button) {
+      if (button.textContent === cityInput) {
+        isDuplicate = true;
+      }
+    });
+  
+    // adds new button if it's not a duplicate and if the max limit has not been reached
+    if (!isDuplicate) {
+      var cityButton = document.createElement("button");
+      cityButton.classList.add('btn', 'btn-secondary', 'btn-lg', 'mb-2');
+      cityButton.textContent = cityInput;
+      cityButtonContainer.appendChild(cityButton);
+  
+      // removes oldest button if max limit of 6 is reached
+      if (buttons.length >= 6) {
+        cityButtonContainer.removeChild(buttons[0]);
+      }
     }
   }
-
-  // This function handles events where a movie button is clicked
-  $("#add-movie").on("click", function(event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var movie = $("#movie-input").val().trim();
-
-    // Adding movie from the textbox to our array
-    movies.push(movie);
-
-    // Calling renderButtons which handles the processing of our movie array
-    renderSearchHistoryButtons();
-  });
-
-  // Adding a click event listener to all elements with a class of "movie-btn"
-  $(document).on("click", ".movie-btn", displayMovieInfo);
-
-  // Calling the renderButtons function to display the intial buttons
-  renderSearchHistoryButtons();
-
-
-
   
-// //save user input to local storage
-function retrieveGeocoordinates(){
-  var cityCoordinates = $(this).attr("data-cityCoordinates");
-  var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={2e7ad0266ed734e297938c3bcc22afc5}"
 
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    $("#movies-view").empty();
 
    
-
-
 
 
 
